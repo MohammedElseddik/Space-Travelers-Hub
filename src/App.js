6b2e491f209c33cc50cec42b47c4608ff0f8ec86
unknown <sntakirutimana72@gmail.com>
2022-07-18 17:07:43 +0200

    Add style for active navlink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navigation/NavBar';
+import NotFound from './components/NotFound';
 
 import './App.css';
 
@@ -7,6 +8,12 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <Routes>
+        <Route exact path="/" element={<NotFound />} />
+        <Route path="/profile" element={<NotFound />} />
+        <Route path="/missions" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
