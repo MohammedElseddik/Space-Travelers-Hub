d544034e9d572647c9720463d808c2466ee8b170
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-19 22:31:47 +0100

    Create mission reducer

---
+++ src/redux/features/missionSlice.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+const url = "https://api.spacexdata.com/v3/missions";
+
+const initialState = {
+  missionData: [],
+};
+
+const missionSlice = createSlice({
+  name: "mission",
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
