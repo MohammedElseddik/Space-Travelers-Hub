3020958ccf1a93bb6d0abf08a239dcba5a8a6a5f
unknown <sntakirutimana72@gmail.com>
2022-07-20 19:39:53 +0200

    Fix linter errors

--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import RocketItem from '../RocketItem';
 import { fetchRockets } from '../../redux/features/rocketSlice';
 import { getState } from '../../redux/effects/rocketEffects';
+import RocketItem from '../RocketItem';
 
 const Rockets = () => {
   const { error, rockets, status } = useSelector(getState);
