98ba4f1625f1179ac364b5fd78e1e9e9aca08b93
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-20 09:19:56 +0100

    Fetch mission data

--- src/redux/features/missionSlice.js
+++ src/redux/features/missionSlice.js
@@ -1,18 +1,44 @@
-import axios from "axios";
+import axios from 'axios';
 
-const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit');
 
-const url = "https://api.spacexdata.com/v3/missions";
+const url = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
   missionData: [],
 };
 
+export const fetchMissionData = createAsyncThunk(
+  'mission/fetchMissionData',
+  async () => {
+    try {
+      const response = await axios(url);
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+);
+
 const missionSlice = createSlice({
-  name: "mission",
+  name: 'mission',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [fetchMissionData.pending]: (state) => {
+      state.isLoading = true;
+    },
+
+    [fetchMissionData.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.missionData = action.payload;
+    },
+
+    [fetchMissionData.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
 });
 
 export default missionSlice.reducer;
