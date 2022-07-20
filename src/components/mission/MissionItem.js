18e6f1c9e400a9e460d9421a6c342f7d2986d850
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-20 08:33:48 +0100

    Create Mission and MissionItem components

---
+++ src/components/mission/MissionItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MissionItem = () => {
+  return (
+    <article>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </article>
+  );
+};
+
+export default MissionItem;
---
+++ src/components/mission/Missions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MissionItem from "./MissionItem";
+
+const Missions = () => {
+  return (
+    <main>
+      <MissionItem />
+    </main>
+  );
+};
+
+export default Missions;
