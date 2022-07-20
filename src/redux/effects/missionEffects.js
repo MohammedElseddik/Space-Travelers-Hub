37338ffe515dd8a3ed7e626e3390dcee3b20c49c
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-20 15:23:13 +0100

    update

--- src/redux/effects/missionEffects.js
+++ src/redux/effects/missionEffects.js
@@ -1,11 +1,9 @@
-const missionEffectAfterFetch = ({ payload }) => {
-  payload.map(({ mission_name, mission_id, mission_description }) => {
-    return {
-      mission_id,
-      mission_name,
-      mission_description,
-    };
-  });
+export const missionEffectAfterFetch = ({ payload }) => {
+  console.log(payload);
+  payload.map((mission) => ({
+    mission_id: mission.mission_id,
+    mission_name: mission.mission_name,
+    mission_description: mission.description,
+  }));
 };
-
 export default missionEffectAfterFetch;
