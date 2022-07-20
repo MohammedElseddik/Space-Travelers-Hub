574ef6e08a341eed74aa2cb4f10b78eb047528aa
unknown <sntakirutimana72@gmail.com>
2022-07-20 10:53:54 +0200

    Create basic missions table

--- src/components/pages/Missions.js
+++ src/components/pages/Missions.js
@@ -1,3 +1,18 @@
-const Missions = () => <div>Missions page under construction</div>;
+const Missions = () => (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th aria-label="action" />
+        </tr>
+      </thead>
+      <tbody />
+      <tfoot />
+    </table>
+  </div>
+);
 
 export default Missions;
