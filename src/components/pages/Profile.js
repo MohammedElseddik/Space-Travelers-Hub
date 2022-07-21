12cfc076ca481db2af0e97f24f66a105f1d912f4
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-21 10:50:02 +0100

    Filtered and Display reserved rockets

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,3 +1,26 @@
-const Profile = () => <div>Profile page under construction</div>;
+import { useSelector } from 'react-redux';
+
+import { selectAllJoinedRockets } from '../../redux/effects/rocketEffects';
+
+const Profile = () => {
+  const { rockets } = useSelector(selectAllJoinedRockets);
+  const isRocktesEmapty = rockets.length === 0;
+
+  return (
+    <section>
+      <div>
+        <h2>My Rockets</h2>
+        {!isRocktesEmapty && (
+          <ul>
+            {rockets.map(({ name, id }) => (
+              <li key={id}>{name}</li>
+            ))}
+          </ul>
+        )}
+        {isRocktesEmapty && <p>No Missions</p>}
+      </div>
+    </section>
+  );
+};
 
 export default Profile;
